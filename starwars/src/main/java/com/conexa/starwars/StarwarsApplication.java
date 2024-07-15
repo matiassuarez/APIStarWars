55a9e5a130b75763718de2e10ba6f5aff45de1b1
Matias <matias.suarez@sancorsalud.com.ar>
2024-07-15 18:19:53 -0300

    Initial commit

---
+++ starwars/src/main/java/com/conexa/starwars/StarwarsApplication.java
@@ -0,0 +1,12 @@
+package com.conexa.starwars;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StarwarsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StarwarsApplication.class, args);
+	}
+}
